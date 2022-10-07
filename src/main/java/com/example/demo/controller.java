88f469a2751d652f1549019423d3a164632bcc49
Ernesto Pareja <epareja2@gmail.com>
2022-10-06 20:39:34 -0500

    Arreglo de funcion para mostrar en WEB

--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -1,19 +1,39 @@
 package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.sound.midi.Soundbank;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import javax.servlet.http.HttpServletRequest;
  
 @SpringBootApplication
 
-@Controller
+@RestController
 
 
 public class controller {
 
     @RequestMapping("/app")
+    
     public String outData () {
-        System.out.println("Hola mundo cruel!");
-        return "outData ";
+        return "Hola mundo cruel!";
+    }
+
+    @RequestMapping("/ayuda")
+    public String ayuda (HttpServletRequest req) {
+        String params = StreamSupport.stream(
+            ((Iterable<String>) () -> req.getParameterNames().asIterator()).spliterator(), false)
+            .map(pName -> pName + '=' + req.getParameter(pName))
+            .collect(Collectors.joining("&"));
+            return req.getRequestURI() + '?' + params;      
     }
 }
