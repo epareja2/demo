bb7e9dab182d7c9674474fdb2075d10089067908
Ernesto Pareja <epareja2@gmail.com>
2022-10-06 21:42:11 -0500

    Ajuste a las librerías necesarias

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.io.*;
-import java.net.http.HttpClient;
 
 @SpringBootApplication
 public class DemoApplication {
--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -1,5 +1,4 @@
 package com.example.demo;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
