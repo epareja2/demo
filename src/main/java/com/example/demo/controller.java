5f5737266fb41e43adf018d42a319aa9977f5bb3
Ernesto Pareja <epareja2@gmail.com>
2022-10-09 20:59:31 -0500

    Cambio en el nombre archivo

--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -1,4 +1,5 @@
 package com.example.demo;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,27 +11,26 @@ import java.util.stream.StreamSupport;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
- 
+
 @SpringBootApplication
 
 @RestController
 
-
 public class controller {
 
     @RequestMapping("/app")
-    
-    public String outData () {
+
+    public String outData() {
         return "Hola mundo cruel!";
     }
 
-    @RequestMapping("/ayuda")
-    public String ayuda (HttpServletRequest req, HttpServletResponse resp) {
+    @RequestMapping("/testurl")
+    public String ayuda(HttpServletRequest req, HttpServletResponse resp) {
         String params = StreamSupport.stream(
-            ((Iterable<String>) () -> req.getParameterNames().asIterator()).spliterator(), false)
-            .map(pName -> pName + '=' + req.getParameter(pName))
-            .collect(Collectors.joining("&"));
-            // resp.getWriter().write(params);
-            return "<h1>" + req.getRequestURI() + '?' + HtmlUtils.htmlEscape(params) + "</h1>";      
+                ((Iterable<String>) () -> req.getParameterNames().asIterator()).spliterator(), false)
+                .map(pName -> pName + '=' + req.getParameter(pName))
+                .collect(Collectors.joining("&"));
+        // resp.getWriter().write(params);
+        return "<h1>" + req.getRequestURI() + '?' + HtmlUtils.htmlEscape(params) + "</h1>";
     }
 }
